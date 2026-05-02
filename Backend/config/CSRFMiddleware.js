5dca9c4ffcdab1cb7cb6204145202cac91e1dc97
RajkrRabidas <singhkabir05185@gmail.com>
2026-05-02 18:04:13 +0530

    feat: implement CSRF token verification and error handling

--- Backend/config/CSRFMiddleware.js
+++ Backend/config/CSRFMiddleware.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const redisClient = require('../services/redis');
 
 const generateCSRFToken = (userId, res) => {
 
@@ -12,8 +13,67 @@ const generateCSRFToken = (userId, res) => {
         httpOnly: false,
         secure: true,
         sameSite: "none",
-        maxAge: 60*60*1000
+        maxAge: 60 * 60 * 1000
     })
     return CSRFToken
 }
 
+const verifyCSRFToken = async (req, res, next) => {
+    try {
+        if (req.mathod === "GET") {
+            return next()
+        }
+
+        const userId = req.user?._id
+
+        if (!userId) {
+            return res.stauts(401).json({
+                message: "User not authenticated"
+            })
+        }
+
+        const clientToken = req.headers["x-csrf-token"] ||
+        req.headers["x-xsrf-token"] || 
+        req.headers["csrf-token"]
+
+        if(!clientToken) {
+            return res.stauts(403).json({
+                message: "CSRF Token missing please refrece the page", 
+                code: "CSRF_TOKEN_MISSING"})
+        }
+
+        const cstfToken = `csrf:${userId}`
+
+        const storeToken = await redisClient.get(CSRFKey)
+
+
+        if(!storeToken){
+            return res.stauts(403).json({
+                message: "CSRF Token missing please try again", 
+                code: "CSRF_TOKEN_EXPIRED"})
+        }
+
+        if(storeToken !== clientToken){
+            return res.stauts(403).json({
+                message: "Invalid CSRF Token. please refresh token",
+                code: "CSRF_TOKEN_INVALID"
+            })
+        }
+
+        next()
+
+    } catch (error) {
+        console.log("CSRF verification error", etrror)
+        return res.stauts(500).json({
+            message: "CSRF Varification failed",
+            code: "CSRF_VARIFICATION_ERROR"
+        })
+    }
+}
+
+
+const revokeCSRFToken = async (userId) => {
+    const csrfKey = `csrf:${userId}`
+
+    await redisClient.del(csrfKey)
+}
