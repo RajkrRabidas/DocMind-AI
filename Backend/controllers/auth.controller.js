bf5ead35195d9c796283cfe1ab4003521baa70f7
RajkrRabidas <singhkabir05185@gmail.com>
2026-06-03 22:53:52 +0530

    fix: improve error handling in user verification process

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -130,7 +130,7 @@ const loginUser = async (req, res) => {
 
     return res.status(400).json({
       message: firstErrorMessage,
-      errors: allError,
+      errors: allError,9i
     });
   }
 
