e1575443b7f6246e5f3f976e9c511ba3600d8146
RajkrRabidas <singhkabir05185@gmail.com>
2026-06-23 19:14:59 +0530

    feat: implement document deletion functionality in MyDocuments component

--- Frontend/src/pages/MyDocuments.jsx
+++ Frontend/src/pages/MyDocuments.jsx
@@ -21,11 +21,23 @@ import {
 } from "lucide-react";
 import api from "../apiIntersepters";
 import { AppData } from "../context/AppContext";
+import { useParams } from "react-router-dom";
 
 const MyDocuments = () => {
   const { documents, docsLoading, fetchDocuments } = AppData();
 
   // const pdfUrl = `http://localhost:5000/uploads/${doc.fileUrl}`;
+  const params = useParams()
+
+  const deleteDocument = async (docId) => {
+    try {
+      const { data } = await api.delete(`/api/documents/delete/${docId}`);
+
+      fetchDocuments(); // Refresh the documents list after deletion
+    } catch (error) {
+      console.error("Error deleting document:", error);
+    }
+  };
 
   useEffect(() => {
     fetchDocuments();
@@ -50,7 +62,7 @@ const MyDocuments = () => {
         ) : (
           documents.map((doc, index) => (
             <div
-              key={doc.name}
+              key={doc._id}
               className={`grid gap-3 p-4 sm:grid-cols-[1fr_auto_auto] sm:items-center ${
                 index !== documents.length - 1
                   ? "border-b border-slate-200"
@@ -82,16 +94,13 @@ const MyDocuments = () => {
                       "_blank",
                     )
                   }
-                  className="rounded-lg border border-slate-200 px-3 py-2 text-xs font-semibold text-slate-700"
+                  className="rounded-lg border border-slate-200 px-3 py-2 text-xs font-semibold text-slate-700 hover:bg-slate-100 cursor-pointer"
                 >
                   Open
                 </div>
-                {doc.canDelete && (
-                  <div className="flex items-center gap-1 rounded-lg border border-red-100 px-3 py-2 text-xs font-semibold text-red-600">
+                  <div onClick={() => deleteDocument(doc._id)} className="flex items-center gap-1 rounded-lg border border-red-100 px-3 py-2 text-xs font-semibold text-red-600 hover:bg-red-100 cursor-pointer">
                     <Trash2 size={14} />
-                    Delete
                   </div>
-                )}
               </div>
             </div>
           ))
